6193ac51851efb7ee003df827f3432d17fce0e7b
Martin Povišer <povik@protonmail.com>
2021-10-26 12:16:18 +0200

    package/petitboot: Extend the dummy platform code
    At least read the model string from devicetree. Also force petitboot
    to use the dummy platform instead of arm64.

--- package/petitboot/petitboot.mk
+++ package/petitboot/petitboot.mk
@@ -15,7 +15,7 @@ PETITBOOT_LICENSE_FILES = COPYING
 
 PETITBOOT_CONF_OPTS += --with-ncurses --without-twin-x11 --without-twin-fbdev \
 	      --localstatedir=/var \
-	      --enable-crypt \
+	      --enable-crypt --disable-platform-auto \
 	      HOST_PROG_KEXEC=/usr/sbin/kexec \
 	      HOST_PROG_SHUTDOWN=/usr/libexec/petitboot/bb-kexec-reboot \
 	      $(if $(BR2_PACKAGE_BUSYBOX),--with-tftp=busybox --enable-busybox)
